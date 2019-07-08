78ea8c5f8cb435609a84cfdfae182d07fce7a925
drcat <dmitry.kotin@kreatetechnology.com>
2019-07-08 18:45:39 +0300

    A project skeleton with redux, router connected.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import "bootstrap/dist/css/bootstrap.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = function(state = initialState, action) {
+  return state
+}
+
+
+const store = createStore(reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
+
+serviceWorker.unregister()
